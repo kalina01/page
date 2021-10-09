d9e08f850df82b7637e1c5ec7649f5d26af751e8
Полина Калиниченко <polina.a.kalinichenko@mail.ru>
2021-10-09 22:35:17 +1000

    page

---
+++ myhobby/script.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.menubutton')) {
+      var dropdowns = document.getElementsByClassName("menucontent");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+
+var piano = new Audio("/sound/piano.mp3");
+var gitara = new Audio("/sound/gitara.mp3");
+var flute = new Audio("/sound/flute.mp3");
